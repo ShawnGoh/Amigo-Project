b88319a00df22c2d4caaf8d17c6b257042864507
ShawnGoh <goh_shawn1998@hotmail.com>
2020-11-30 15:38:59 +0800

    add dependecies etc

---
+++ app/src/main/java/com/example/infosys1d_amigoproject/HomeScreen.java
@@ -0,0 +1,14 @@
+package com.example.infosys1d_amigoproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
