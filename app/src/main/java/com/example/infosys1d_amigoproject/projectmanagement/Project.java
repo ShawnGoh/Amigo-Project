441d017a701e7a1d70d95cea5cda50bf40ad344d
khaizon <61576319+khaizon@users.noreply.github.com>
2020-11-30 20:31:08 +0800

    k

--- app/src/main/java/com/example/infosys1d_amigoproject/projectmanagement/Project.java
+++ app/src/main/java/com/example/infosys1d_amigoproject/projectmanagement/Project.java
@@ -1,4 +1,48 @@
 package com.example.infosys1d_amigoproject.projectmanagement;
 
+import android.net.Uri;
+
+import com.example.infosys1d_amigoproject.models.users_display;
+
+import java.util.ArrayList;
+
 public class Project {
+    private Uri thumbnail;
+    private String projectitle, projectdescription;
+    private ArrayList<String> skillsrequired;
+    private ArrayList<users_display> usersinProject;
+    private users_display createdby;
+
+    public Project(Uri thumbnail, String projectitle, String projectdescription, ArrayList<String> skillsrequired, ArrayList<users_display> usersinProject, users_display createdby) {
+        this.thumbnail = thumbnail;
+        this.projectitle = projectitle;
+        this.projectdescription = projectdescription;
+        this.skillsrequired = skillsrequired;
+        this.usersinProject = usersinProject;
+        this.createdby = createdby;
+    }
+
+    public Uri getThumbnail() {
+        return thumbnail;
+    }
+
+    public String getProjectitle() {
+        return projectitle;
+    }
+
+    public String getProjectdescription() {
+        return projectdescription;
+    }
+
+    public ArrayList<String> getSkillsrequired() {
+        return skillsrequired;
+    }
+
+    public ArrayList<users_display> getUsersinProject() {
+        return usersinProject;
+    }
+
+    public users_display getCreatedby() {
+        return createdby;
+    }
 }
