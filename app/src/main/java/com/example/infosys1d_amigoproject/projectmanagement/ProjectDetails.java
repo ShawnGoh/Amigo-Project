b1a863e3589cab5d62ca67b6b33865370d2179fb
Qi Yan <sqiyan@gmail.com>
2020-12-02 02:58:36 +0800

    Set up extended FAB for chat and apply

--- app/src/main/java/com/example/infosys1d_amigoproject/projectmanagement/ProjectDetails.java
+++ app/src/main/java/com/example/infosys1d_amigoproject/projectmanagement/ProjectDetails.java
@@ -3,6 +3,8 @@ package com.example.infosys1d_amigoproject.projectmanagement;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,19 +25,34 @@ public class ProjectDetails extends AppCompatActivity {
     CollapsingToolbarLayout mCollapsingToolbarLayout;
     StorageReference storageReference,projectref;
     ImageView imageView;
+    Button clicktoChat;
+    Button applytoJoin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_details);
         mCollapsingToolbarLayout = findViewById(R.id.collapse);
-        mCollapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.collapsingToolbarLayoutTitleColor);
-        mCollapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.collapsingToolbarLayoutTitleColor);
-
+//        mCollapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.collapsingToolbarLayoutTitleColor);
+//        mCollapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.collapsingToolbarLayoutTitleColor);
 
         Intent intent = getIntent();
         String text = intent.getStringExtra("ProjectID");
         imageView = findViewById(R.id.project_picture);
+        clicktoChat = findViewById(R.id.clicktoChat);
+        clicktoChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // intent to chat with project creator
+            }
+        });
+        applytoJoin = findViewById(R.id.applytoJoin);
+        applytoJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // intent to project application fragment OR fillable dialogue
+            }
+        });
 
         storageReference = FirebaseStorage.getInstance().getReference();
         projectref = storageReference.child("users/"+ FirebaseAuth.getInstance().getCurrentUser().getUid()+"/profile.jpg");
