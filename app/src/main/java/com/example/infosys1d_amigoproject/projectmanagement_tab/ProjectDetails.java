011d3b240fac3f734eade8c1e57161c97c28dd03
khaizon <61576319+khaizon@users.noreply.github.com>
2020-12-02 11:17:18 +0800

    more project details cahnges

--- app/src/main/java/com/example/infosys1d_amigoproject/projectmanagement_tab/ProjectDetails.java
+++ app/src/main/java/com/example/infosys1d_amigoproject/projectmanagement_tab/ProjectDetails.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.infosys1d_amigoproject.R;
+import com.example.infosys1d_amigoproject.Utils.FirebaseMethods;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.firebase.auth.FirebaseAuth;
@@ -25,9 +27,10 @@ import com.squareup.picasso.Picasso;
 
 public class ProjectDetails extends AppCompatActivity {
     CollapsingToolbarLayout mCollapsingToolbarLayout;
-    StorageReference storageReference,projectref;
-    DatabaseReference myref;
-    ImageView imageView;
+    DatabaseReference myref,userref;
+    ImageView imageView, createdby_pic;
+    TextView createdby_text,project_description;
+    Project project;
     Button clicktoChat, applytoJoin, back;
 
     @Override
@@ -35,7 +38,10 @@ public class ProjectDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_details);
         mCollapsingToolbarLayout = findViewById(R.id.collapse);
-
+        createdby_text = findViewById(R.id.projectCreator);
+        project_description = findViewById(R.id.projectDescription);
+        createdby_pic = findViewById(R.id.createdby_pic);
+        FirebaseMethods firebaseMethods = new FirebaseMethods(getApplicationContext());
         imageView = findViewById(R.id.project_picture);
         Intent intent = getIntent();
         String project_id = intent.getStringExtra("ProjectID");
@@ -43,17 +49,37 @@ public class ProjectDetails extends AppCompatActivity {
         myref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Project project = snapshot.getValue(Project.class);
+                project = snapshot.getValue(Project.class);
                 System.out.println(project.getThumbnail()+"   123456");
                 Picasso.get().load(project.getThumbnail()).into(imageView);
+                mCollapsingToolbarLayout.setTitle(project.getProjectitle());
+
+
+
+                userref = FirebaseDatabase.getInstance().getReference().child("users_display").child(project_id);
+                userref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        project = snapshot.getValue(user.class);
+                        System.out.println(project.getThumbnail()+"   123456");
+                        Picasso.get().load(project.getThumbnail()).into(imageView);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                })
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
-//        mCollapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.collapsingToolbarLayoutTitleColor);
-//        mCollapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.collapsingToolbarLayoutTitleColor);
+        mCollapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.theme_expanded);
+        mCollapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.theme_collapsed);
+
+
 
         clicktoChat = findViewById(R.id.clicktoChat);
         clicktoChat.setOnClickListener(new View.OnClickListener() {
