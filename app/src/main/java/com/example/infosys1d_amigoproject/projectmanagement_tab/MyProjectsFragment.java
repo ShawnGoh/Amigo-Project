578becda7aa5dbcc8fdcf8d4087d8d5a92869fc0
bryaniiiii <51448923+bryaniiiii@users.noreply.github.com>
2020-12-02 17:48:20 +0800

    added my projects

--- app/src/main/java/com/example/infosys1d_amigoproject/projectmanagement_tab/MyProjectsFragment.java
+++ app/src/main/java/com/example/infosys1d_amigoproject/projectmanagement_tab/MyProjectsFragment.java
@@ -1,65 +1,166 @@
 package com.example.infosys1d_amigoproject.projectmanagement_tab;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.infosys1d_amigoproject.MyAdapter;
 import com.example.infosys1d_amigoproject.R;
+import com.example.infosys1d_amigoproject.Utils.FirebaseMethods;
+import com.example.infosys1d_amigoproject.models.Userdataretrieval;
+import com.example.infosys1d_amigoproject.models.users_display;
+import com.example.infosys1d_amigoproject.models.users_private;
+import com.google.android.material.chip.Chip;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyProjectsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class MyProjectsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    MyAdapter myAdapter;
+    private TextView mTitleName;
+    private static final String TAG = "profilefragment";
+    //Firebase Database
+    private FirebaseDatabase mFirebasedatabase;
+    private DatabaseReference databaseReference;
+    StorageReference storageReference = FirebaseStorage.getInstance().getReference();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
+    //Firebase Auth
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthstatelistner;
+    private FirebaseMethods firebaseMethods;
+    users_display user;
+
+    RecyclerView recyclerView;
     public MyProjectsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyProjectsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyProjectsFragment newInstance(String param1, String param2) {
-        MyProjectsFragment fragment = new MyProjectsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_projects, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_my_projects, container, false);
+        mTitleName = view.findViewById(R.id.Title);
+        recyclerView = view.findViewById(R.id.suggestedRecycler2);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        setupfirebaseauth();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference mref2 = FirebaseDatabase.getInstance().getReference("users_display").child(FirebaseAuth.getInstance().getUid());
+        mref2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                user= snapshot.getValue(users_display.class);
+                mTitleName.setText(user.getName() + "'s Projects");
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        DatabaseReference projref = databaseReference.child("Projects");
+        ArrayList<Project> projectList = new ArrayList<>();
+        myAdapter = new MyAdapter(projectList);
+        recyclerView.setAdapter(myAdapter);
+
+
+
+        projref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                projectList.clear();
+                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+                    Project project = postSnapshot.getValue(Project.class);
+                    for (String userID: project.getUsersinProject()){
+                        if (FirebaseAuth.getInstance().getCurrentUser().getUid() != (userID)){
+                            projectList.add(project);
+                        }
+                    }
+                }
+                myAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: ");
+            }
+        });
+        return view;
+    }
+
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthstatelistner);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(mAuthstatelistner!=null){
+            mAuth.removeAuthStateListener(mAuthstatelistner);
+        }
+    }
+
+    //FirebaseAuth
+    private void setupfirebaseauth(){
+        Log.d(TAG, "Setup FirebaseAuth");
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirebasedatabase = FirebaseDatabase.getInstance();
+        databaseReference = mFirebasedatabase.getReference();;
+
+        //check if user is sign in
+        mAuthstatelistner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user  = firebaseAuth.getCurrentUser();
+
+
+                if(user !=null){
+                    //user is signed in
+                    Log.d(TAG, "onAuthStateChanged: signed_in" +user.getUid());
+                }
+                else{
+                    //user is signed out
+                    Log.d(TAG, "onAuthStateChanged: signed_out");
+                }
+
+            }
+        };
+
+
     }
 }
