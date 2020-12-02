c7651e8ece2efc998891764f85b0fbcd4d712bf8
bryaniiiii <51448923+bryaniiiii@users.noreply.github.com>
2020-12-03 00:55:20 +0800

    Profile Setup First Three Intents

--- app/src/main/java/com/example/infosys1d_amigoproject/profilesetup/ProfileSetupProfilePic.java
+++ app/src/main/java/com/example/infosys1d_amigoproject/profilesetup/ProfileSetupProfilePic.java
@@ -12,5 +12,8 @@ public class ProfileSetupProfilePic extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_setup_profile_pic);
+        System.out.println(getIntent().getStringExtra("About Me")+ "92383312");
+        System.out.println(getIntent().getStringExtra("Looking For")+ "92383312");
+        System.out.println(getIntent().getStringExtra("Skills")+ "92383312");
     }
 }
