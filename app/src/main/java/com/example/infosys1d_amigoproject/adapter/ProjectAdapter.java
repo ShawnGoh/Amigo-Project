7c6ec14d502d1d91e10d190c061723ef02dda331
khaizon <61576319+khaizon@users.noreply.github.com>
2021-01-29 13:26:20 +0800

    trying out infinite scroll

---
+++ app/src/main/java/com/example/infosys1d_amigoproject/adapter/ProjectAdapter.java
@@ -0,0 +1,28 @@
+package com.example.infosys1d_amigoproject.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ProjectAdapter extends RecyclerView.Adapter<MyAdapter.myholder>{
+
+    public ProjectAdapter() {
+    }
+
+    @NonNull
+    @Override
+    public MyAdapter.myholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.myholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/infosys1d_amigoproject/projectmanagement_tab/InfintieScrolling.java
@@ -0,0 +1,16 @@
+package com.example.infosys1d_amigoproject.projectmanagement_tab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.infosys1d_amigoproject.R;
+
+public class InfintieScrolling extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infintie_scrolling);
+    }
+}
